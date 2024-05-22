0ed1c98121208da71b989c51da9cb89774b72778
Ahmed Hassan <ahmedkiller0234@gmail.com>
2024-05-22 09:17:57 +0500

    first commit

---
+++ src/app/user/loading.tsx
@@ -0,0 +1,15 @@
+
+
+
+function Loading() {
+  
+  return (
+    <section className="py-24">
+        <div className="container">
+            <h2 className="text-2xl">Loading.....</h2>
+        </div>
+    </section>
+  )
+}
+
+export default Loading
