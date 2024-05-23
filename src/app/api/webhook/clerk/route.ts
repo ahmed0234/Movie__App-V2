1f8e1f75f7011ae014824739c31d2cbb9c2463be
Ahmed Hassan <ahmedkiller0234@gmail.com>
2024-05-23 23:27:53 +0500

    last

--- src/app/api/webhook/clerk/route.ts
+++ src/app/api/webhook/clerk/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
+export async function GET(req: NextRequest) {
   return NextResponse.json({ success: true });
 }
