82e41ddc3f70bf0e86f74f269a6701f29309ae4d
Ahmed Hassan <ahmedkiller0234@gmail.com>
2024-05-23 23:15:06 +0500

    webhook clerk updated

--- src/app/api/webhook/clerk/page.tsx
+++ src/app/api/webhook/clerk/page.tsx
@@ -1,5 +1,5 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-    
+  return NextResponse.json({ success: true });
 }
