01378bca8101d2853962c9cdf01f96d6b3936657
Ahmed Hassan <ahmedkiller0234@gmail.com>
2024-05-24 07:13:43 +0500

    fixing prisma client initialization

--- src/app/api/prisma/route.ts
+++ src/app/api/prisma/route.ts
@@ -1,11 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import prisma from "@/utils/db";
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient();
 
 export async function GET(request: NextRequest) {
   const newUser = await prisma.user.findFirst({
     where: {
-      email: "ahmedkiller0234@gmail.com",
+      email: "hassan1234@gmail.com",
     },
   });
 
